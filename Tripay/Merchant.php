ab0261294081df2969161b8f0a8a7c3ef77a284f
Rizal Arfani <arfanirizal22@gmail.com>
2022-02-24 15:56:07 +0700

    method list transaksi

--- Tripay/Merchant.php
+++ Tripay/Merchant.php
@@ -15,7 +15,7 @@ class Merchant
         return Requestor::get(
             $url,
             Config::$apiKey,
-            $code
+            $code,
         );
     }
 
@@ -28,11 +28,34 @@ class Merchant
      */
     public static function getKalkulatorBiaya($ammout, $code = null)
     {
-        ($code) ? $data = ['amount' => (int)$ammout, 'code' => $code] : ['amount' => (int)$ammout];
+        ($code) ? $payload = ['amount' => (int)$ammout, 'code' => $code] : ['amount' => (int)$ammout];
         return Requestor::get(
-            Config::getBaseUrl() . 'merchant/fee-calculator?' . http_build_query($data),
+            Config::getBaseUrl() . 'merchant/fee-calculator?' . http_build_query($payload),
             Config::$apiKey,
-            $data,
+            $payload,
+        );
+    }
+
+    /**
+     * Get Daftar Transaksi
+     * @param int $page
+     * @param int $per_page
+     * @param String $sort asc,desc
+     * @param String $code
+     * @param String $status
+     */
+    public static function getListTransaksi($page = 1, $per_page = 25, $sort = 'asc', $status = null)
+    {
+        $payload = [
+            'page' => $page,
+            'per_page' => $per_page,
+            'sort' => $sort,
+            'status' => $status
+        ];
+        return Requestor::get(
+            Config::getBaseUrl() . 'merchant/transactions?' . http_build_query($payload),
+            Config::$apiKey,
+            $payload,
         );
     }
 }
